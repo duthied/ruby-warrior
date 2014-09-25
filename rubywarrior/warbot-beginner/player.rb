ecbca6bca302c97d55f7f6080fb0646d06ddeef2
Devlon Duthie <duthied@gmail.com>
2014-09-25 11:43:48 -0700

    Level 2 completed

--- rubywarrior/warbot-beginner/player.rb
+++ rubywarrior/warbot-beginner/player.rb
@@ -1,59 +1,55 @@
 
 class Player
-  
-  def play_turn(warrior)
-    warrior.walk!
-  end
 
-  # def play_turn(warrior)
-  #   # @health ||= warrior.health
-  #   # @position_initialized ||= false
-  #   # @warrior ||= warrior
+  def play_turn(warrior)
+    # @health ||= warrior.health
+    # @position_initialized ||= false
+    # @warrior ||= warrior
     
-  #   # @position_initialized ? feel_forward(warrior) : feel_backward(warrior)
-  #   feel_forward(warrior)
-  #   # @health = warrior.health
-  # end
+    # @position_initialized ? feel_forward(warrior) : feel_backward(warrior)
+    feel_forward(warrior)
+    # @health = warrior.health
+  end
   
-  # def feel_backward(warrior)
-  #   case
-  #   when warrior.feel(:backward).empty?
-  #     warrior.walk!(:backward)
+  def feel_backward(warrior)
+    case
+    when warrior.feel(:backward).empty?
+      warrior.walk!(:backward)
     
-  #   when warrior.feel(:backward).captive?
-  #     warrior.rescue!(:backward)
+    # when warrior.feel(:backward).captive?
+    #   warrior.rescue!(:backward)
       
-  #   when warrior.feel(:backward).wall?
-  #     @position_initialized = true
+    # when warrior.feel(:backward).wall?
+    #   @position_initialized = true
         
-  #   end
-  # end
+    end
+  end
   
-  # def feel_forward(warrior)
-  #   byebug
-  #   case
-  #   when warrior.feel.captive?
-  #     warrior.rescue!
+  def feel_forward(warrior)
+    # byebug
+    case
+    # when warrior.feel.captive?
+    #   warrior.rescue!
       
-  #   when warrior.feel.enemy?
-  #     warrior.attack!
+    when warrior.feel.enemy?
+      warrior.attack!
       
-  #   when warrior.feel.empty? && wounded? && !under_attack?
-  #     warrior.rest!
+    # when warrior.feel.empty? && wounded? && !under_attack?
+    #   warrior.rest!
     
-  #   when warrior.feel.empty?
-  #     warrior.walk!
+    when warrior.feel.empty?
+      warrior.walk!
      
-  #   end
-  # end
+    end
+  end
   
-  # def under_attack?
-  #   @health > @warrior.health
-  # end
+  def under_attack?
+    @health > @warrior.health
+  end
   
-  # def wounded?
-  #   @warrior.health < 20
-  # end
+  def wounded?
+    @warrior.health < 20
+  end
   
 end
   
