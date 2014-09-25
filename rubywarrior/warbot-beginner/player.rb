96c1abe8fda46dc3ff69fcbde623564a68d066f5
Devlon Duthie <duthied@gmail.com>
2014-09-25 09:17:11 -0700

    Init

---
+++ rubywarrior/warbot-beginner/player.rb
@@ -0,0 +1,54 @@
+
+class Player
+  
+  def play_turn(warrior)
+    @health ||= warrior.health
+    @position_initialized ||= false
+    # @warrior ||= warrior
+    
+    @position_initialized ? feel_forward(warrior) : feel_backward(warrior)
+    
+    @health = warrior.health
+  end
+  
+  def feel_backward(warrior)
+    case
+    when @warrior.feel(:backward).empty?
+      @warrior.walk!(:backward)
+    
+    when @warrior.feel(:backward).captive?
+      @warrior.rescue!(:backward)
+      
+    when @warrior.feel(:backward).wall?
+      @position_initialized = true
+        
+    end
+  end
+  
+  def feel_forward
+    case
+    when @warrior.feel.captive?
+      @warrior.rescue!
+      
+    when @warrior.feel.enemy?
+      @warrior.attack!
+      
+    when @warrior.feel.empty? && wounded? && !under_attack?
+      @warrior.rest!
+    
+    when @warrior.feel.empty?
+      @warrior.walk!
+     
+    end
+  end
+  
+  def under_attack?
+    @health > @warrior.health
+  end
+  
+  def wounded?
+    @warrior.health < 20
+  end
+  
+end
+  
